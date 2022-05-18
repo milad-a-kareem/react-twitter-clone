fd995105bb145e538658bfd3732b7c11b2fe84db
Milad <milad.kareem@gmail.com>
2022-05-18 06:38:42 +0300

    List section done

--- src/pages/Lists.js
+++ src/pages/Lists.js
@@ -9,6 +9,7 @@ import PageTitle from "../components/PageTitle";
 import { ReactComponent as BackIcon } from "../assets/icons/outline/left-arrow.svg";
 import { ReactComponent as AddListIcon } from "../assets/icons/outline/addList.svg";
 import { ReactComponent as EllipsisIcon } from "../assets/icons/outline/ellipsis.svg";
+import { ReactComponent as ListssIcon } from "../assets/icons/outline/lists.svg";
 
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
@@ -16,6 +17,8 @@ import TrendsForYou from "../components/TrendsForYou";
 import WhoToFollow from "../components/WhoToFollow";
 import FooterLinks from "../components/FooterLinks";
 import Seperator from "../components/Sperator";
+
+import users from "../data/users";
 
 function Lists() {
   const user = useSelector((state) => state.user);
@@ -63,9 +66,27 @@ function Lists() {
 
         <Seperator />
         <h2 className="text-xl font-bold px-4 py-3">Discover new Lists</h2>
+        <ListItem
+          bgColor="#fdba74"
+          iconColor="#fb923c"
+          user={users[0]}
+          title="ReactJS"
+        />
+        <ListItem
+          bgColor="#86efac"
+          iconColor="#4ade80"
+          user={users[1]}
+          title="Angular"
+        />
+        <ListItem
+          bgColor="#38bdf8"
+          iconColor="#7dd3fc"
+          user={users[6]}
+          title="KeyU"
+        />
 
         <Seperator />
-        <h2 className="text-xl font-bold px-4 py-3">Your Lists</h2>
+        <h2 className="text-xl font-bold px-4 py-3 ">Your Lists</h2>
         <div className="text-sm p-8 text-black/80">
           You haven't created or followed any Lists. When you do, they'll show
           up here.
@@ -102,3 +123,40 @@ function Lists() {
 }
 
 export default Lists;
+
+function ListItem({ user, title, iconColor, bgColor }) {
+  const classes = `
+  w-12 
+  h-12 
+  rounded-xl 
+  overflow-hidden 
+  shrink-0 
+  p-[10px]
+  `;
+  return (
+    <div className="flex px-4 py-3 items-center hover:bg-xlight-gray overflow-hidden">
+      <div className="flex gap-4 w-10 min-w-0 max-w-full grow shrink overflow-hidden">
+        <div className={classes} style={{ backgroundColor: bgColor }}>
+          <ListssIcon style={{ fill: iconColor }} />
+        </div>
+        <div className="flex flex-col items-start text-sm justify-center overflow-hidden">
+          <div className="font-bold">{title}</div>
+          <div className="flex gap-1 items-center text-[13px] min-w-0 max-w-full  shrink  justify-start overflow-hidden">
+            <div className="w-4 h-4 rounded-full overflow-hidden bg-blue shrink-0">
+              <img src={user.profileImage} alt="" className="w-full h-full" />
+            </div>
+            <div className="font-bold truncate shrink min-w-0 break-words ">
+              <span>{user.display_name}</span>
+            </div>
+            <div className="truncate shrink min-w-0 break-words ">
+              <span>@{user.username}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex ml-4 rounded-full bg-black hover:bg-black/90 cursor-pointer px-4 text-white font-bold text-sm h-[32px] items-center">
+        Follow
+      </div>
+    </div>
+  );
+}
